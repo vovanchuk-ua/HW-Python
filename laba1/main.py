9aaa3d94809753232fead9434db8c5251dc518d7
Volodymyr Kukharskyi <147499243+vovanchuk-ua@users.noreply.github.com>
2024-10-02 17:45:58 +0300

    Update main.py

--- laba1/main.py
+++ laba1/main.py
@@ -1,10 +1,8 @@
-import math
-
 x = float(input("Введіть x: "))
 
 def findY(x):
     if x <= -1:
-        y = (1 + abs(x)) / math.sqrt(1 + x + x**2)
+        y = (1 + abs(x)) / (1 + x + x**2)**1/3
     else:
         y = (1 + math.cos(x)**4) / (3 + x)
     return y
